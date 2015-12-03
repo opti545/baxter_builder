dc9195d80d3674625117c8ae05b8b25f4f098f66
josmiranda <opti545@gmail.com>
2015-12-03 13:51:13 -0600

    WIP: Added drop location, new vision pose

--- scripts/baxter_mover.py
+++ scripts/baxter_mover.py
@@ -38,6 +38,11 @@ def init():
             "ExternalTools/left/PositionKinematicsNode/IKService",
             SolvePositionIK)
 
+    global ik_service_right
+    ik_service_right = rospy.ServiceProxy(
+            "ExternalTools/right/PositionKinematicsNode/IKService",
+            SolvePositionIK)
+    
     global stopflag
     stopflag = False
     #Taken from the MoveIt Tutorials
@@ -51,11 +56,47 @@ def init():
     global group
     group = MoveGroupCommander("left_arm")
     
+    
+    global right_group
+    right_group = MoveGroupCommander("right_arm")
+    pose_right = Pose()
+    pose_right.position = Point(0.587, -0.579, 0.480)
+    pose_right.orientation = Quaternion(0.029, 0.998, -0.046, 0.015)
+    request_pose_right(pose_right)
+    
+
     global left_gripper
     left_gripper = baxter_interface.Gripper('left')
     left_gripper.calibrate()
 
     move_to_vision()
+
+
+def request_pose_right(pose):
+    # Set stamped pose
+    pose_stamped = PoseStamped()
+    pose_stamped.pose = pose
+    pose_stamped.header.frame_id = "base"
+    pose_stamped.header.stamp = rospy.Time.now()
+
+    # Create IK request 
+    ik_request = SolvePositionIKRequest()
+    ik_request.pose_stamp.append(pose_stamped)
+
+    # Request service
+    try:
+        rospy.wait_for_service("ExternalTools/right/PositionKinematicsNode/IKService", 5.0)
+        ik_response = ik_service_right(ik_request)
+    except (rospy.ServiceException, rospy.ROSException), error_message:
+        rospy.logerr("Service request failed: %r" %(error_message))
+        sys.exit("ERROR - move_to_observe - Failed to append pose")
+    if ik_response.isValid[0]:
+        limb_joints = dict(zip(ik_response.joints[0].name, ik_response.joints[0].position))
+        right_group.set_joint_value_target(limb_joints)
+        plan2= right_group.plan()
+        rospy.sleep(5)
+        right_group.go(wait=True)
+
 
 def request_pose(pose):
     # Set stamped pose
@@ -77,27 +118,37 @@ def request_pose(pose):
         sys.exit("ERROR - move_to_observe - Failed to append pose")
     if ik_response.isValid[0]:
         limb_joints = dict(zip(ik_response.joints[0].name, ik_response.joints[0].position))
-        group.clear_pose_targets()
         group.set_joint_value_target(limb_joints)
         plan2= group.plan()
         rospy.sleep(5)
         group.go(wait=True)
-    return
-
-
-
 
 
 def move_to_vision():
     # Set pose
     pose = Pose()
     pose.orientation = Quaternion(0.00, 1.0, 0.00, 0.00)
-    pose.position = Point(0.623, 0.414, 0.201)
+    pose.position = Point(0.765, 0.035, 0.435)
 
     # Request service
     request_pose(pose)
 
+def move_to_box():
+    pose = Pose()
+    pose.orientation = Quaternion(0.00, 1.0, 0.00, 0.00)
+    pose.position = Point(0.675, -0.138, 0.412)
 
+
+def pick_and_place():
+    pose = Pose()
+    pose.position = Point(xpos, ypos, zpos)
+    pose.orientation = Quaternion(0.00, 1.00, 0.00, 0.00)
+
+    request_pose(pose)
+    left_gripper.close()
+
+    move_to_vision()
+    move_to_box()
 
 
 
